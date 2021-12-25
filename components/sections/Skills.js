59b727e80f9cd5336958bb39e0312682c7502571
Martin Sonnberger <martin.sonnberger@icloud.com>
2021-12-25 15:31:36 +0100

    remove bottom margin

--- components/sections/Skills.js
+++ components/sections/Skills.js
@@ -4,7 +4,7 @@ const Skills = ({ skills }) => {
   return (
     <section className="mt-32">
       <Heading>Tech I&apos;ve worked with</Heading>
-      <div className="mt-10 mb-20 max-w-2xl flex-wrap flex gap-x-4 gap-y-8">
+      <div className="mt-10 max-w-2xl flex-wrap flex gap-x-4 gap-y-8">
         {skills.map((skill) => (
           <SkillBubble key={skill} skill={skill} />
         ))}
