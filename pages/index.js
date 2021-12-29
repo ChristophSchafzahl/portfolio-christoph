dabd96b55e851f33c41615e001de71a1a3998a0d
Martin Sonnberger <martin.sonnberger@icloud.com>
2021-12-29 18:40:14 +0100

    add head to index page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
 import { Hero, About, Skills, Projects, Modeling, Contact, Footer } from '@components/index';
+import Head from 'next/head';
 import { skills, socials, projects, models } from '/data/data';
 
 export default function Home() {
   return (
     <div className="min-h-screen flex items-center flex-col bg-teal-darker text-teal-50">
+      <Head>
+        <title>Christoph Schafzahl</title>
+        <meta property="og:title" content="Christoph Schafzahl" key="title" />
+      </Head>
       <Hero socials={socials} />
       <main className="max-w-5xl w-full px-8">
         <About />
